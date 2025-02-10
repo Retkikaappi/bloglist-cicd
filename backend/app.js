1c91297613f113ace64f3a67c5bb88f5bab6628b
Joonas Nuolikoski <joonas.nuolikoski@gmail.com>
2025-02-10 17:34:48 +0200

    outlined pipeline

--- backend/app.js
+++ backend/app.js
@@ -12,16 +12,19 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-mongoose.connect(config.MONGODB_URI)
+mongoose
+  .connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
   })
-  .catch(err => {
+  .catch((err) => {
     logger.error('error connecting to MongoDB', err.message)
   })
 
 app.use(cors())
-app.use(express.static('dist'))
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('dist'))
+}
 app.use(express.json())
 app.use(middleware.requestLog)
 app.use(middleware.tokenExtractor)
@@ -30,7 +33,7 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
-if(process.env.NODE_ENV === 'test'){
+if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/tests')
   app.use('/api/testing', testingRouter)
 }
